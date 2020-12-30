7f0aacad4fbca1515ee59c7d54c3299575e3a895
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2020-12-30 10:18:06 +0200

    Finished running all tests

--- ex_4.py
+++ ex_4.py
@@ -24,11 +24,11 @@ class MyDataSet(data.Dataset):
 
     def __getitem__(self, index):
         x = self.__x[index]
-        y = self.__y[index]
         x = np.asarray(x).astype(np.uint8).reshape(28, 28)
         if self.__transform:
             x = self.__transform(x)
         if self.__y is not None:
+            y = self.__y[index]
             return x, y
         return x
 
@@ -47,65 +47,71 @@ def split_validation_train(train_x, train_y):
     return train_x, train_y, validation_x, validation_y
 
 
-def show_graph_accuracy(loss_train, loss_val):
-    pass
-
-
-def show_graph_loss(loss_train, loss_val):
-    lt = np.array(loss_train)
-    lv = np.array(loss_val)
+def show_graph(train_res, test_res, what_kind):
+    lt = np.array(train_res)
+    lv = np.array(test_res)
     epoch = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    plt.plot(epoch, lt, 'g', label='Training loss')
-    plt.plot(epoch, lv, 'b', label='validation loss')
-    plt.title('Training and Validation loss')
+    plt.plot(epoch, lt, 'g', label='Training {}'.format(what_kind))
+    plt.plot(epoch, lv, 'b', label='validation {}'.format(what_kind))
+    plt.title('Training and Validation {}'.format(what_kind))
     plt.xlabel('Epochs')
-    plt.ylabel('Loss')
+    plt.ylabel(what_kind)
     plt.legend()
     plt.show()
 
 
-def run_model(model, train_loader, validation_loader):
+def run_model(model, train_loader, test_loader):
     loss_trains = []
     loss_tests = []
+    accuracy_train = []
+    accuracy_test = []
     for e in range(1, EPOCH + 1):
-        loss_trains.append(train(model, train_loader))
-        loss_tests.append(test(model, validation_loader))
-    show_graph_loss(loss_trains, loss_tests)
+        loss_tr, acc_tr = train(model, train_loader)
+        loss_te, acc_te = test(model, test_loader)
+        loss_trains.append(loss_tr / len(train_loader.sampler))
+        loss_tests.append(loss_te / len(test_loader.sampler))
+        accuracy_train.append(acc_tr)
+        accuracy_test.append(acc_te)
+    show_graph(loss_trains, loss_tests, 'Loss')
+    show_graph(accuracy_train, accuracy_test, 'Accuracy')
 
 
 def train(model, train_loader):
     model.train()
     losses = 0
+    correct = 0
     for batch_idx, (data_, labels) in enumerate(train_loader):
         model.optimizer.zero_grad()
         output = model(data_)
         loss = F.nll_loss(output, labels)
-        losses += F.nll_loss(output, labels, size_average=False).item()
+        losses += F.nll_loss(output, labels, reduction="mean").item()
+        pred = output.max(1, keepdim=True)[1]
+        correct += pred.eq(labels.view_as(pred)).cpu().sum()
         loss.backward()
         model.optimizer.step()
-    return losses
+    return losses, (100. * correct / len(train_loader.sampler))
 
 
-def test(model, validation_loader):
+def test(model, test_loader):
     model.eval()
     test_loss = 0
     tmp_loss = 0
     correct = 0
     with torch.no_grad():
-        for data_, target in validation_loader:
+        for data_, target in test_loader:
             output = model(data_)
             # sum up batch loss
-            tmp_loss += F.nll_loss(output, target, size_average=False).item()
+            tmp_loss += F.nll_loss(output, target, reduction="mean").item()
             test_loss += F.nll_loss(output, target, reduction="mean").item()
             # get index of the max log - probability
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).cpu().sum()
-    test_loss /= len(validation_loader.dataset)
+    test_loss /= len(test_loader.dataset)
     print("\nTests set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(test_loss, correct,
-                                                                                  len(validation_loader.dataset),
+                                                                                  len(test_loader.dataset),
                                                                                   100. * correct / len(
-                                                                                      validation_loader.sampler)))
-    return tmp_loss
+                                                                                      test_loader.sampler)))
+    return tmp_loss, (100. * correct / len(test_loader.sampler))
 
 
 def create_loaders(train_x, train_y, validation_x, validation_y):
@@ -113,40 +119,41 @@ def create_loaders(train_x, train_y, validation_x, validation_y):
         [torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))])
     train_set = MyDataSet(train_x, train_y, transforms)
     validation_set = MyDataSet(validation_x, validation_y, transforms)
+
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
     validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=64, shuffle=True)
     return train_loader, validation_loader
 
 
 def run_models(train_loader, validation_loader):
-    print("*********************** MODEL A ******************** ")
-    model_a = ModelA(IMAGE_SIZE)
-    run_model(model_a, train_loader, validation_loader)
     print("******************** MODEL B *********************** ")
     model_b = ModelB(IMAGE_SIZE)
     run_model(model_b, train_loader, validation_loader)
-    print("******************** MODEL C *********************** ")
-    model_c = ModelC(IMAGE_SIZE)
-    run_model(model_c, train_loader, validation_loader)
-    print("******************** MODEL D *********************** ")
-    model_d = ModelD(IMAGE_SIZE)
-    run_model(model_d, train_loader, validation_loader)
-    print("******************** MODEL E *********************** ")
-    model_e = ModelE(IMAGE_SIZE)
-    run_model(model_e, train_loader, validation_loader)
-    print("******************** MODEL F *********************** ")
-    model_f = ModelF(IMAGE_SIZE)
-    run_model(model_f, train_loader, validation_loader)
+
+
+def create_original_loaders():
+    train_set = torchvision.datasets.\
+        FashionMNIST(
+            root="./data", download=True, transform=torchvision.transforms.ToTensor())
+    test_set = torchvision.datasets.\
+        FashionMNIST(
+            root="./data", train=False,
+            download=True, transform=torchvision.transforms.ToTensor())
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=100)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=100)
+    return train_loader, test_loader
+
 
 
 def main():
     try:
         train_x = np.loadtxt(sys.argv[1])
         train_y = np.loadtxt(sys.argv[2], dtype="int64")
-        test_x = np.loadtxt(sys.argv[3])
         train_x, train_y, validation_x, validation_y = split_validation_train(train_x, train_y)
-        train_loader, validation_loader = create_loaders(train_x, train_y, validation_x, validation_y)
-        run_models(train_loader, validation_loader)
+        train_loader, validation_loader = create_loaders(
+            train_x, train_y, validation_x, validation_y)
+        train_fashion_loader, test_fashion_loader = create_original_loaders()
+        run_models(train_fashion_loader, test_fashion_loader)
     except IndexError as e:
         print(e)
         exit(-1)
